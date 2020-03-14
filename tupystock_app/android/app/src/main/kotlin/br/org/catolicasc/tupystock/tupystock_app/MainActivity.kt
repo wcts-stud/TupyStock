53da3af6426b587286cdbe65052501c264ee594a
Willyam Castro <willyam.castro@catolicasc.org.br>
2020-03-13 21:33:50 -0300

    app default

---
+++ tupystock_app/android/app/src/main/kotlin/br/org/catolicasc/tupystock/tupystock_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.org.catolicasc.tupystock.tupystock_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
